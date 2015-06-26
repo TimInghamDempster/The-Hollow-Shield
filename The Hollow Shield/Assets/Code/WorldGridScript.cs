09473436e9404673fa60059dc843bb24b121408c
Tim Ingham-Dempster <tim_ingham_dempster@hotmail.co.uk>
2015-06-26 22:24:07 +0100

    [TID] Add a world grid and editor script to generate grid tiles, add test hex.

---
+++ The Hollow Shield/Assets/Code/WorldGridScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldGridScript : MonoBehaviour {
+
+	public int TileCountX;
+	public int TileCountY;
+
+	WorldTileScript[,] tiles;
+
+	public void Create()
+	{
+		tiles = new WorldTileScript[TileCountX, TileCountY];
+	}
+
+	public void AddTile(int x, int y, WorldTileScript tile)
+	{
+		tiles[x, y] = tile;
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ The Hollow Shield/Assets/Editor/Tools.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MenuItems
+{
+	[MenuItem("Hollow Shield Tools/Create Hex Grid")]
+	private static void NewMenuOption()
+	{
+		WorldGridScript grid = GameObject.Find("WorldGrid").GetComponent<WorldGridScript>();
+		grid.Create();
+
+		float twoRootThree = 1.0f * Mathf.Sqrt(3.0f);
+		for (int x = 0; x < grid.TileCountX; x++)
+		{
+			for (int y = 0; y < grid.TileCountY; y++)
+			{
+				GameObject go = new GameObject("HexTile" + x + y);
+
+				float yPos = y * twoRootThree;
+
+				if(x % 2 == 0)
+				{
+					yPos += twoRootThree / 2.0f;
+				}
+
+				go.transform.position = new Vector3(x * 1.5f, 0, yPos);
+
+				WorldTileScript tile = go.AddComponent<WorldTileScript>();
+				
+				grid.AddTile(x, y, tile);
+			}
+		}
+	}
+}
