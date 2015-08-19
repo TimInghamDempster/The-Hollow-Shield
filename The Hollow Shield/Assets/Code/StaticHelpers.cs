ed89dd665b18c1c7fefd9a2547c24c7152b8b648
Tim Ingham-Dempster <tim_ingham_dempster@hotmail.co.uk>
2015-08-20 00:30:03 +0100

    [TID] Add missed files.

---
+++ The Hollow Shield/Assets/Code/StaticHelpers.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class StaticHelpers {
+
+	public static int Clamp( int value, int min, int max )
+	{
+		return (value < min) ? min : (value > max) ? max : value;
+	}
+
+	public static void FloodFill(int GridSizeX, int GridSizeY, List<List<WorldTileScript>> StartingLists, out List<List<WorldTileScript>> OutputLists)
+	{
+		OutputLists = new List<List<WorldTileScript>>();
+		for(int i = 0; i < StartingLists.Count; i++)
+		{
+			OutputLists.Add(new List<WorldTileScript>());
+		}
+
+		bool[,] closed = new bool[GridSizeX, GridSizeY];
+
+		List<List<WorldTileScript>> openLists = new List<List<WorldTileScript>>();
+		List<List<WorldTileScript>> openListsToWriteTo = new List<List<WorldTileScript>>();
+
+		foreach(List<WorldTileScript> list in StartingLists)
+		{
+			List<WorldTileScript> newList = new List<WorldTileScript>();
+
+			for(int i = 0; i < list.Count; i++)
+			{
+				WorldTileScript tile = list[i];
+				newList.Add(tile);
+				closed[tile.x, tile.y] = true;
+			}
+			openLists.Add(newList);
+			openListsToWriteTo.Add(new List<WorldTileScript>());
+		}
+
+		bool finished = false;
+
+		while(!finished)
+		{
+			finished = true;
+
+			bool openListsEmpty = false;
+
+			while(!openListsEmpty)
+			{
+				openListsEmpty = true;
+				for(int i = 0; i < openLists.Count; i++)
+				{
+					List<WorldTileScript> list = openLists[i];
+
+					if(list.Count > 0)
+					{
+						finished = false;
+						openListsEmpty = false;
+
+						WorldTileScript tile = list[list.Count - 1];
+						WorldTileScript[] neighbours = tile.GetNeighbours();
+						list.RemoveAt(list.Count - 1);
+						OutputLists[i].Add(tile);
+
+						for(int j = 0; j < neighbours.Length; j++)
+						{
+							WorldTileScript neighbour = neighbours[j];
+							if(!closed[neighbour.x, neighbour.y])
+							{
+								closed[neighbour.x, neighbour.y] = true;
+								openListsToWriteTo[i].Add(neighbour);
+							}
+						}
+					}
+				}
+			}
+
+			List<List<WorldTileScript>> temp   = openLists;
+			openLists = openListsToWriteTo;
+			openListsToWriteTo = temp;
+
+			foreach(List<WorldTileScript> list in openListsToWriteTo)
+			{
+				list.Clear();
+			}
+		}
+
+		return;
+	}
+}
