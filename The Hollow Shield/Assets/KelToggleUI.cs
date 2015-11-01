e647adde331e014477ae0a7629ffc0d278cc9f97
KelShaded <Kel@Kel-Shaded.com>
2015-11-01 18:11:16 +0000

    raven and ux tests
    raven and ux tests, lightened some assets

---
+++ The Hollow Shield/Assets/KelToggleUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KelToggleUI : MonoBehaviour
+{
+	public GameObject localUiToggle = null;
+	public GameObject infoUiToggle = null;
+	public int toggle = 1;
+
+	private void OnMouseDown()
+	{
+		if (toggle != 0) 
+		{
+			localUiToggle.gameObject.SetActive (true);
+			toggle --;
+			//print ("if worked");
+		}
+
+		else
+		{
+			localUiToggle.gameObject.SetActive (false);
+			toggle++;
+			//print ("else worked");
+		}
+
+		infoUiToggle.gameObject.SetActive (true);
+	}
+
+
+
+	private void OnMouseUp()
+	{	
+		infoUiToggle.gameObject.SetActive (false) ;
+		//print("mouseup worked");
+	}
+}
